b225af184cac351a20518f82db7f426855df21d8
Darwin Lara <lrdarwin75@hotmail.com>
2024-01-18 20:45:52 -0500

    Terminado

--- Actividad1/js/solitario.js
+++ Actividad1/js/solitario.js
@@ -1,27 +1,26 @@
 /***** INICIO DECLARACIÓN DE VARIABLES GLOBALES *****/
 
 // Variables simples
-let barajaClick = null; //Contiene la baraja que fue dada click en primer lugar
-
+let barajaInicio = 1;
+let barajaFin = 2;
+let tapeteInicio = null;
+let contadorMovimientos = 0;
+let indicadorMovimiento = document.getElementById("indicador_movimientos");
+let indicadorTiempo = document.getElementById("indicador_tiempo");
 
 // Array de palos
 let palos = ["viu", "cua", "hex", "cir"];
 
 // Array de colores
 let colores = {
-   viu: "naranja",
-   cua: "naranja",
-   hex: "gris",
-   cir: "gris",
+  viu: "naranja",
+  cua: "naranja",
+  hex: "gris",
+  cir: "gris",
 };
 
-// Array de número de cartas
-//let numeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-// En las pruebas iniciales solo se trabajará con cuatro cartas por palo:
-let numeros = [9, 10, 11, 12];
-
 // paso (top y left) en pixeles de una carta a la siguiente en un mazo
-let paso = 5;
+let paso = 10;
 
 // Tapetes
 let tapeteInicial = document.getElementById("inicial");
@@ -34,19 +33,8 @@ let tapeteReceptor4 = document.getElementById("receptor4");
 // Mazos
 let mazoInicial = [];
 let mazoBarajado = [];
-let mazoSobrantes = [];
-let mazoReceptor1 = [];
-let mazoReceptor2 = [];
-let mazoReceptor3 = [];
-let mazoReceptor4 = [];
 
 // Contadores de cartas
-let contInicial = document.getElementById("contador_inicial");
-let contSobrantes = document.getElementById("contador_sobrantes");
-let contReceptor1 = document.getElementById("contador_receptor1");
-let contReceptor2 = document.getElementById("contador_receptor2");
-let contReceptor3 = document.getElementById("contador_receptor3");
-let contReceptor4 = document.getElementById("contador_receptor4");
 let contMovimientos = document.getElementById("contador_movimientos");
 
 // Tiempo
@@ -54,117 +42,155 @@ let contTiempo = document.getElementById("contador_tiempo"); // span cuenta tiem
 let segundos = 0; // cuenta de segundos
 let temporizador = null; // manejador del temporizador
 
-// Boton de reinicio
-let btnReiniciar = document.getElementById("boton_reiniciar"); // btn reinicio juego
-
 /***** FIN DECLARACIÓN DE VARIABLES GLOBALES *****/
 
-// Rutina asociada a boton reset
-/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-
-btnReiniciar.onclick = () => {
-   //tapeteInicial = [];
-   mazoInicial = [];
-   mazoBarajado = [];
-
-   ComenzarJuego();
-  
-};
-
 // El juego arranca ya al cargar la página: no se espera a reiniciar
-/*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
+ComenzarJuego();
 
 // Desarrollo del comienzo de juego
 function ComenzarJuego() {
-  /* Crear baraja, es decir crear el mazoInicial. Este será un array cuyos 
-	elementos serán elementos HTML <img>, siendo cada uno de ellos una carta.
-	Sugerencia: en dos bucles for, bárranse los "palos" y los "numeros", formando
-	oportunamente el nombre del fichero png que contiene a la carta (recuérdese poner
-	el path correcto en la URL asociada al atributo src de <img>). Una vez creado
-	el elemento img, inclúyase como elemento del array mazoInicial. 
-	*/
+  // llamado a la funcion de limpieza de variables
+  limpiezaVariables();
+  
+  // llamado a la funcion de creacion del mazo inicial
+  CrearMazoInicial();
 
-  /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-  // Creacion del mazo inicial
-  k=1;
-   for (let i = 1; i <= 12; i++) {
+  // llamado a la funcion para barajar el mazo inicial
+  mazoBarajado = BarajarMazo(mazoInicial);
+
+  // Llamado a la funcion para colocar el mazo en el tapete inicial
+  ColocarTapeteInicial(mazoBarajado);
+
+  // Llamado a la funcion que arranca el conteo de tiempo
+  arrancarTiempo();
+} 
+
+/*** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! **/
+/*** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FUNCIONES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! **/
+/*** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! **/
+
+//Funcion de limpieza de variables
+function limpiezaVariables()
+{
+   mazoInicial = [];
+   mazoBarajado = [];
+   tapeteInicial.innerHTML = "";
+   tapeteSobrantes.innerHTML = "";
+   tapeteReceptor1.innerHTML = "";
+   tapeteReceptor2.innerHTML = "";
+   tapeteReceptor3.innerHTML = "";
+   tapeteReceptor4.innerHTML = "";
+   contMovimientos.innerHTML = 0;
+}
+
+// Funcion para crear el mazo inicial
+function CrearMazoInicial() 
+{
+   k = 1;
+   for (let i = barajaInicio; i <= barajaFin; i++) {
       for (let j = 0; j < palos.length; j++) {
-         const baraja = {
+         const baraja = 
+         {
             id: "baraja-" + k,
             numero: i,
             color: colores[palos[j]],
             palo: palos[j],
             img: `./imagenes/baraja/${i}-${palos[j]}.png`,
             cara_frontal: false,
-            
+            tapete: "inicial",
          };
          k += 1;
          mazoInicial.push(baraja);
       }
    }
+ }
 
-  // Barajar el mazo inicial
-   mazoBarajado = BarajarMazo(mazoInicial);
+ // Funcion para barajar aleatoriamente el mazo inicial
+function BarajarMazo(mazo) 
+{
+   const nuevoMazo = mazo
+     .map((value) => ({ value, sort: Math.random() }))
+     .sort((a, b) => a.sort - b.sort)
+     .map(({ value }) => value);
+   return nuevoMazo;
+ }
 
-  // Colocar el mazo barajado en el tapete inicial
-   for (let i = 0; i < mazoBarajado.length; i++) {
-      
-      const ultimo = i === mazoBarajado.length-1;
+// Funcion para colocar el mazo en el tapete inicial
+function ColocarTapeteInicial(mazo) 
+{
+   for (let i = 0; i < mazoBarajado.length; i++) 
+   {
+      const ultimo = i === mazoBarajado.length - 1;
       const baraja = mazoBarajado[i];
-      if(ultimo){
+      if (ultimo) 
+      {
          baraja.cara_frontal = true;
       }
-      const barajaHTML = CrearBarajaHTML (baraja);
+      const barajaHTML = CrearBarajaHTML(baraja);
       barajaHTML.style.top = `${i * paso}px`;
       barajaHTML.style.left = `${i * paso}px`;
       tapeteInicial.appendChild(barajaHTML);
    }
+   const contInic = document.createElement("span");
+   tapeteInicial.appendChild(contInic);
+   contInic.setAttribute("id", "contador_inicial");
+   let contInicial = document.getElementById("contador_inicial");
+   contInicial.innerHTML = tapeteInicial.children.length - 1;
+   contInicial.classList.add("contador");
 
-   // Colocar el mazo barajado en el tapete inicial
+   ContadorCrear(tapeteSobrantes, "contador_sobrantes");
+   ContadorCrear(tapeteReceptor1, "contador_receptor1");
+   ContadorCrear(tapeteReceptor2, "contador_receptor2");
+   ContadorCrear(tapeteReceptor3, "contador_receptor3");
+   ContadorCrear(tapeteReceptor4, "contador_receptor4");
+}
 
+// Funcion para crear la baraja en HTML
+function CrearBarajaHTML(baraja) 
+{
+   const imagen = document.createElement("img");
+   if (baraja.cara_frontal === true) 
+   {
+      imagen.src = baraja.img;
+   } else 
+   {
+    imagen.src = "../../../cardGame/Actividad1/imagenes/baraja/lomo.png";
+   }
+   imagen.setAttribute("id", baraja.id);
+   imagen.dataset.numero = baraja.numero;
+   imagen.dataset.color = baraja.color;
+   imagen.dataset.palo = baraja.palo;
+   imagen.dataset.img = baraja.img;
+   imagen.dataset.cara_frontal = baraja.cara_frontal;
+   imagen.dataset.tapete = baraja.tapete;
+   imagen.classList.add("estilobaraja");
+   imagen.setAttribute("draggable", false);
+   imagen.onclick = () => 
+   {
+      ComprobarBarajaClick(imagen);
+   };
+   return imagen;
+}
 
-   
-  // Puesta a cero de contadores de mazos
-  /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
+// Funcion que verificar a que carta se dio un click
+function ComprobarBarajaClick(baraja) 
+{
+   if (baraja.dataset.cara_frontal == "true") {
+      const selecionado = document.querySelectorAll("img.estilobarajaclick");
+      if (selecionado.length > 0) 
+      {
+         selecionado[0].className = " estilobaraja";
+      }
+      
+      HabilitarMovimiento(baraja);
+   }
+}
 
-  // Arrancar el conteo de tiempo
-  /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-} // comenzarJuego
-
-
-/**
-	Se debe encargar de arrancar el temporizador: cada 1000 ms se
-	debe ejecutar una función que a partir de la cuenta autoincrementada
-	de los segundos (segundos totales) visualice el tiempo oportunamente con el 
-	format hh:mm:ss en el contador adecuado.
-
-	Para descomponer los segundos en horas, minutos y segundos pueden emplearse
-	las siguientes igualdades:
-
-	segundos = truncar (   segundos_totales % (60)                 )
-	minutos  = truncar ( ( segundos_totales % (60*60) )     / 60   )
-	horas    = truncar ( ( segundos_totales % (60*60*24)) ) / 3600 )
-
-	donde % denota la operación módulo (resto de la división entre los operadores)
-
-	Así, por ejemplo, si la cuenta de segundos totales es de 134 s, entonces será:
-	   00:02:14
-
-	Como existe la posibilidad de "resetear" el juego en cualquier momento, hay que 
-	evitar que exista más de un temporizador simultáneo, por lo que debería guardarse
-	el resultado de la llamada a setInterval en alguna variable para llamar oportunamente
-	a clearInterval en su caso.   
-*/
-
-/*** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! **/
-/*** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! FUNCIONES !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! **/
-/*** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! **/
-
-// Da comienzo al temporizador
+// Funcion que arranca el conteo de tiempo
 function arrancarTiempo() {
-  /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
    if (temporizador) clearInterval(temporizador);
-   let hms = function () {
+   let hms = function () 
+   {
       let seg = Math.trunc(segundos % 60);
       let min = Math.trunc((segundos % 3600) / 60);
       let hor = Math.trunc((segundos % 86400) / 3600);
@@ -182,111 +208,168 @@ function arrancarTiempo() {
    temporizador = setInterval(hms, 1000);
 }
 
-/**
-	Si mazo es un array de elementos <img>, en esta rutina debe ser
-	reordenado aleatoriamente. Al ser un array un objeto, se pasa
-	por referencia, de modo que si se altera el orden de dicho array
-	dentro de la rutina, esto aparecerá reflejado fuera de la misma.
-*/
+/*** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! **/
+/*** !!!!!!!!!!!!!!!!!!!!!!!!! FUNCIONES PARA EL MOVIMIENTO !!!!!!!!!!!!!!!!!!!!!!!!! **/
+/*** !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! **/
 
-// Funcion para barajar aleatoriamente el mazo de barajas
-function BarajarMazo(mazo) {
-   const nuevoMazo = mazo
-      .map((value) => ({ value, sort: Math.random() }))
-      .sort((a, b) => a.sort - b.sort)
-      .map(({ value }) => value);
-   return nuevoMazo;
+// Funcion cuando se comienza a mover 
+function ComienzoMovimiento(event) 
+{
+  event.dataTransfer.setData("text", event.target.id);
 }
 
-// Funcion para crear la baraja en HTML
-function CrearBarajaHTML(baraja) {
-   const imagen = document.createElement("img");
+//Funcion que permite el movimeinto
+function PermitirMovimiento(event) 
+{
+  event.preventDefault();
+}
+
+// Funcion cuando se termina de mover
+function SoltarMovimiento(event) 
+{
+   event.preventDefault();
+   var idBaraja = event.dataTransfer.getData("text");
+   var barajaMovida = document.getElementById(idBaraja);
+   tapeteInicio = barajaMovida.offsetParent.id;
+   var tapeteFinal = event.target;
+
+   switch (tapeteFinal.id) {
+      case "sobrantes":
+         barajaMovida = document.getElementById(idBaraja);
+         AceptaBaraja(tapeteFinal, barajaMovida);
+         MostrarContadores(tapeteSobrantes, "contador_sobrantes");
+         MostrarContadores(tapeteInicial, "contador_inicial");
+         break;
+      case "receptor1":
+         Reglas (tapeteReceptor1,tapeteFinal,barajaMovida,"contador_receptor1")
+         break;
+      case "receptor2":
+         Reglas (tapeteReceptor2,tapeteFinal,barajaMovida,"contador_receptor2")
+         break;
+      case "receptor3":
+         Reglas (tapeteReceptor3,tapeteFinal,barajaMovida,"contador_receptor3")
+         break;
+      case "receptor4":
+         Reglas (tapeteReceptor4,tapeteFinal,barajaMovida,"contador_receptor4")
+         break;
+      default:
+   }
+
+   let continicial = document.getElementById("contador_inicial");
+   let contsobrantes = document.getElementById("contador_sobrantes");
+   if(continicial.innerHTML === "0" && contsobrantes.innerHTML === "0")
+   {
+      $("#staticBackdrop").modal("show");
+      
+      indicadorMovimiento.innerHTML = contMovimientos.innerHTML;
+      indicadorTiempo.innerHTML = contTiempo.innerHTML;
+   }
+}
+
+// Funcion de las reglas que rigen el movimiento
+function Reglas (tapete,tapeteFinal,baraja,nombrecontador)
+{
+   const imagenes = tapete.querySelectorAll("img.estilobaraja").length;
+   if (imagenes == 0 && baraja.dataset.numero == barajaFin) 
+   {
+      AceptaBaraja(tapeteFinal, baraja);      
+   } else 
+   {
+      const ultimo = tapete.lastElementChild;
+      const ultimonumero = ultimo.dataset.numero;
+      const ultimocolor = ultimo.dataset.color;
+      if (baraja.dataset.numero == ultimonumero - 1 && baraja.dataset.color != ultimocolor) 
+      {
+         AceptaBaraja(tapeteFinal, baraja);
+      }
+   }
+   MostrarContadores(tapete, nombrecontador);
+   MostrarContadores(tapeteInicial, "contador_inicial");
+   MostrarContadores(tapeteSobrantes, "contador_sobrantes");
+}
+
+// Funcion que acepta el movimiento de la baraja
+function AceptaBaraja(tapeteFinal, barajaMovida) 
+{
+   tapeteFinal.appendChild(barajaMovida);
+   barajaMovida.style.top = `35px`;
+   barajaMovida.style.left = `40px`;
+   barajaMovida.className = " estilobaraja";
+
+   if (tapeteInicio == "inicial") {
+      const ultima = tapeteInicial.lastElementChild.previousElementSibling;
+      if(ultima != null)
+      {
+         ultima.src = ultima.dataset.img;
+         ultima.dataset.cara_frontal = true;
+      }
+   }
+   IngresarMovimiento();
+   HabilitarMovimiento(barajaMovida);
+}
+
+
+// Funcion que crea los indicadores de los contadores
+function ContadorCrear(Tapete, nombre) 
+{   
+   const vcontador = document.createElement("span");
+   Tapete.appendChild(vcontador);
+   vcontador.setAttribute("id", nombre);
+   let contadorhtml = document.getElementById(nombre);
+   contadorhtml.innerHTML = Tapete.children.length - 1;
+   contadorhtml.classList.add("contador");
+}
+
+// Funcion que incrementa y muestra los movimientos
+function IngresarMovimiento() 
+{
+   contadorMovimientos = contadorMovimientos +1;
+   contMovimientos.innerHTML = contadorMovimientos;
+}
+
+// Funcion que solo permite el movimiento entre el Inicial o Sobrantes a los receptores
+function HabilitarMovimiento(barajaMovida) 
+{
+   const variable = document.getElementById(barajaMovida.id)
+   const padre = variable.parentElement.id;
+   if (padre === "inicial"  || padre === "sobrantes")
+   {
+      barajaMovida.className += " estilobarajaclick";
+      barajaMovida.setAttribute("draggable", true);
+      barajaMovida.setAttribute("ondragstart", "ComienzoMovimiento(event)");
+   }else
+   {
+      barajaMovida.className = " estilobaraja";
+      barajaMovida.setAttribute("draggable", false);
+      barajaMovida.removeAttribute("ondragstart");
+   }   
+}
+
+// Funcion que muestra los valores de los contadores
+function MostrarContadores(Tapete, nombre) 
+{
+   let contSobrantes = document.getElementById(nombre);
+   imagenes = Tapete.querySelectorAll("img.estilobaraja").length;
+   contSobrantes.innerHTML = imagenes;
    
-   if (baraja.cara_frontal === true) {
-      imagen.src = baraja.img;
-   } else {
-      imagen.src = "../../../cardGame/Actividad1/imagenes/baraja/lomo.png";
-   }
-   imagen.setAttribute('id', baraja.id); 
-   imagen.dataset.numero = baraja.numero;
-   imagen.dataset.color = baraja.color;
-   imagen.dataset.palo = baraja.palo;
-   imagen.dataset.img = baraja.img;
-   imagen.dataset.cara_frontal = baraja.cara_frontal;
-   imagen.classList.add("estilobaraja");
-   imagen.setAttribute('draggable', false);
-   //Revisar si es necesario
-   imagen.onclick = () => {
-      ComprobarBarajaClick(imagen)
-   }
-   return imagen;
-}
-
-
-// Revisar tal vez no se necesite Verificar que carta se dio un click
-function ComprobarBarajaClick(baraja) {
-   if ( baraja.dataset.cara_frontal == "true") {
-      baraja.className += " estilobarajaclick";
-      baraja.setAttribute('draggable', true);
-      baraja.setAttribute('ondragstart', "drag(event)");
+   if (nombre === "contador_inicial" && imagenes === 0 ) 
+   {
+      VolverBarajas();
+      contSobrantes.innerHTML = Tapete.querySelectorAll("img.estilobaraja").length;
    }
 }
 
-// Funcion para mover las cartas
-function allowDrop(ev) {
-   ev.preventDefault();
+// Funcion que mueve las cartas de sobrantes a inicio 
+function VolverBarajas() 
+{
+   var hijos = tapeteSobrantes.querySelectorAll(".estilobaraja");
+   hijos.forEach(function (img) 
+   {
+    tapeteInicial.appendChild(img);
+   });
 }
 
-function drag(ev) {
-   ev.dataTransfer.setData("text", ev.target.id);
-}
-
-function drop(ev) {
-   ev.preventDefault();
-   var data = ev.dataTransfer.getData("text");
-   var draggedElement = document.getElementById(data);
-   var targetDiv = ev.target;
-
-   if(targetDiv.id === 'sobrantes') {
-      targetDiv.appendChild(draggedElement);
-      baraja.style.top = `0px`;
-      //baraja.style.left = `0px`;
-   }
-}
-
-
-
-
-/**
- 	En el elemento HTML que representa el tapete inicial (variable tapeteInicial)
-	se deben añadir como hijos todos los elementos <img> del array mazo.
-	Antes de añadirlos, se deberían fijar propiedades como la anchura, la posición,
-	coordenadas top y left, algun atributo de tipo data-...
-	Al final se debe ajustar el contador de cartas a la cantidad oportuna
-*/
-function cargarTapeteInicial(mazo) {
-  /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-} // cargarTapeteInicial
-
-/**
- 	Esta función debe incrementar el número correspondiente al contenido textual
-   	del elemento que actúa de contador
-*/
-function incContador(contador) {
-  /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-} // incContador
-
-/**
-	Idem que anterior, pero decrementando 
-*/
-function decContador(contador) {
-  /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! ***/
-} // decContador
-
-/**
-	Similar a las anteriores, pero ajustando la cuenta al
-	valor especificado
-*/
+// Funcion que muestra el tiempo
 function setContador(contador, valor) {
-  /*** !!!!!!!!!!!!!!!!!!! CODIGO !!!!!!!!!!!!!!!!!!!! **/
-} // setContador
+   contador.innerHTML = valor;
+} 
